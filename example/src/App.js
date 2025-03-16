feed4300663b722edc8b5b85bc0af3e8ef886c6d
pioner921227 <alex.shumihin921227@gmail.com>
2025-03-16 21:23:57 +0100

    refactor: refactoring

--- example/src/App.js
+++ example/src/App.js
@@ -2,22 +2,23 @@ import { useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { RNConfig } from 'react-native-config-jsi';
 export default function App() {
-    useEffect(() => {
-        try {
-            RNConfig.get('API_URL');
-        }
-        catch (e) {
-            console.error('ERR', e);
-        }
-    }, []);
-    return (<View style={styles.container}>
+  useEffect(() => {
+    try {
+      RNConfig.get('API_URL');
+    } catch (e) {
+      console.error('ERR', e);
+    }
+  }, []);
+  return (
+    <View style={styles.container}>
       <Text>Result:2</Text>
-    </View>);
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
